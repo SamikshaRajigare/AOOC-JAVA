8874f269cc84e3f897b54e1924e00ae4ce4a4391
SamikshaRajigare <samiksharajigare6354@gmail.com>
2026-03-06 12:58:55 +0530

    Update MathTest.java

--- Exp no. 5 java/MathTest.java
+++ Exp no. 5 java/MathTest.java
@@ -1,23 +1,19 @@
 public class MathTest {
-
-    // Method to perform operations using the built-in Math class
     public void performOperations(double number) {
         System.out.println("Input Number: " + number);
-
-        // Using Java's built-in Math class methods
-        System.out.println("Floor value: " + Math.floor(number)); // Largest integer <= number
-        System.out.println("Ceil value : " + Math.ceil(number));  // Smallest integer >= number
-        System.out.println("Round value: " + Math.round(number)); // Closest integer
+        System.out.println("Floor value: " + Math.floor(number)); 
+        System.out.println("Ceil value : " + Math.ceil(number));  
+        System.out.println("Round value: " + Math.round(number)); 
     }
 
     public static void main(String[] args) {
-        // Corrected class name
+        
         MathTest demo = new MathTest();
-
         System.out.println("--- Testing Math Operations ---");
         demo.performOperations(15.7);
 
         System.out.println("\n--- Testing with another value ---");
         demo.performOperations(12.2);
     }
+
 }
