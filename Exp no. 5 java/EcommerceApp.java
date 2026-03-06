362c62b8cf3497c8a93dc2994c592487d116b51d
SamikshaRajigare <samiksharajigare6354@gmail.com>
2026-03-06 13:00:19 +0530

    Update EcommerceApp.java

--- Exp no. 5 java/EcommerceApp.java
+++ Exp no. 5 java/EcommerceApp.java
@@ -12,8 +12,6 @@ class Product {
         System.out.println("Product: " + productName + " | Price: $" + price);
     }
 }
-
-// Class to store Customer details
 class Customer {
     String customerName;
     String email;
@@ -27,11 +25,9 @@ class Customer {
         System.out.println("Customer: " + customerName + " (" + email + ")");
     }
 }
-
-// Class to handle placing orders and calculating total cost
 class Order {
     void placeOrder(Customer customer, Product product, int quantity) {
-        double totalCost = product.price * quantity; // Calculates total order cost
+        double totalCost = product.price * quantity; 
         
         System.out.println("--- Order Summary ---");
         customer.displayCustomer();
@@ -41,16 +37,12 @@ class Order {
         System.out.println("Status: Order placed successfully!");
     }
 }
-
-
 public class EcommerceApp{
     public static void main(String[] args) {
-        // Creating objects
         Product laptop = new Product("Gaming Laptop", 1200.00);
         Customer user = new Customer("Samiksha", "samikasha@example.com");
         Order myOrder = new Order();
-
-        
         myOrder.placeOrder(user, laptop, 2);
     }
 }
+
