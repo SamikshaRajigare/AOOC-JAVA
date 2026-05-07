9df0a2970700f8d7a9ab146516e01b613b25ea19
SamikshaRajigare <samiksharajigare6354@gmail.com>
2026-05-07 09:57:09 +0530

    Update Q1.java

--- java expt13/Q1.java
+++ java expt13/Q1.java
@@ -3,20 +3,20 @@ import java.sql.*;
 public class DisplayStudent {
     public static void main(String[] args) {
         try {
-            // Load driver
+        
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Connection
+
             Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/college", "root", "password");
 
-            // Statement
+
             Statement stmt = con.createStatement();
 
-            // Query
+        
             ResultSet rs = stmt.executeQuery("SELECT * FROM student");
 
-            // Display data
+        
             while (rs.next()) {
                 System.out.println(
                     rs.getInt("id") + " " +
